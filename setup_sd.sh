02c09b76d3e82ffd6a72c2e365de8ddbc2c788b7
dchung117 <dchung0330@gmail.com>
2023-09-17 02:54:38 +0000

    added code to download counterfeitv2.5 model and vae.

--- setup_sd.sh
+++ setup_sd.sh
@@ -10,6 +10,12 @@ wget -q https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/m
 mv webui.sh ~; cd ~
 source webui.sh
 
+# Install Counterfeit V2.5
+echo "Downloading CounterfeitV2.5 model weights and VAE..."
+cd stable-diffusion-webui
+wget -O models/Stable-diffusion/models/Stable-diffusion/Counterfeit-V2.5.safetensors https://huggingface.co/gsdf/Counterfeit-V2.5/resolve/main/Counterfeit-V2.5.safetensors
+wget -O models/VAE/Counterfeit-V2.5.vae.pt https://huggingface.co/gsdf/Counterfeit-V2.5/resolve/main/Counterfeit-V2.5.vae.pt
+
 echo "Stable diffusion setup successful! Make sure to set COMMANDLINE_ARGS'--share --xformers 
     --theme dark
     --vae-path=/home/ubuntu/stable-diffusion-webui/models/VAE/Counterfeit-V2.5.vae.pt
