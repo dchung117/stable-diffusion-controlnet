2d5f5c998be7ae3c8b635e203b4c255262f52710
David Chung <dchung0330@gmail.com>
2023-09-25 00:08:43 -0400

    Update setup_sd.sh

--- setup_sd.sh
+++ setup_sd.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate stable-diffusion
-
 # Install stable diffusion
 sudo apt install wget git python3 python3-venv libgl1 libglib2.0-0
 wget -q https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh
@@ -15,7 +12,7 @@ echo "Downloading CounterfeitV2.5 model weights and VAE..."
 cd stable-diffusion-webui
 wget -O models/Stable-diffusion/Counterfeit-V2.5.safetensors https://huggingface.co/gsdf/Counterfeit-V2.5/resolve/main/Counterfeit-V2.5.safetensors
 wget -O models/VAE/Counterfeit-V2.5.vae.pt https://huggingface.co/gsdf/Counterfeit-V2.5/resolve/main/Counterfeit-V2.5.vae.pt
-
+wget -O embeddings/EasyNegative.safetensors https://huggingface.co/datasets/gsdf/EasyNegative/blob/main/EasyNegative.safetensors
 echo "Stable diffusion setup successful! Make sure to set COMMANDLINE_ARGS'--share --xformers 
     --theme dark
     --vae-path=/home/ubuntu/stable-diffusion-webui/models/VAE/Counterfeit-V2.5.vae.pt
