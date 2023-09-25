d46ffd7778ca58310b536ad8729f02cd7c7ac825
David Chung <dchung0330@gmail.com>
2023-09-25 00:11:49 -0400

    Update setup_sd.sh

--- setup_sd.sh
+++ setup_sd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate sd
+
 # Install stable diffusion
 sudo apt install wget git python3 python3-venv libgl1 libglib2.0-0
 wget -q https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh
